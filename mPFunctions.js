a64f03f5aedb0f9dd2d11e0c932fe2b07eca60d7
RobbieB <r.blackshaw@hotmail.com>
2022-07-28 13:31:49 +1000

    add main menu in mPFunctions.js

---
+++ mPFunctions.js
@@ -0,0 +1,22 @@
+function mainMenu(){
+        console.log('|======================================|');
+        console.log('|               MusoPlan               |');
+        console.log('|======================================|');
+        console.log('|    Select your choice from below     |');
+        console.log('|       ----------------------         |');
+        console.log('|                                      |');
+        console.log('| 1. Register a Musician               |');
+        console.log('| 2. Create a troupe                   |');
+        console.log('| 3. Add Musician to troupe            |');
+        console.log('| 4. Summary description of troupe     |');
+        console.log('| 5. Detailed description of troupe    |');
+        console.log('| 6. Troupe cost calulation            |');
+        console.log('| 7. Export troupe names to file       |');
+        console.log('| 8. Export troupe details to file     |');
+        console.log('| 9. Exit MusoPlan                     |');
+        console.log('|                                      |');
+        console.log('|                                      |');
+        console.log('|======================================|');
+
+        choice = prompt('Enter your choice [from 1 - 9]: ');
+}
