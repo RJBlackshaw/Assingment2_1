c811fcd31183bb55d9a2ef674dbd2c83c932e203
RobbieB <r.blackshaw@hotmail.com>
2022-07-28 14:46:34 +1000

    add comments and update main menu function to include error message, also add function export

--- mPFunctions.js
+++ mPFunctions.js
@@ -1,4 +1,9 @@
-function mainMenu(){
+//initializes prompt feature into program
+const ps=require('prompt-sync');
+const prompt=ps();
+
+//mainmenu function that will show an error message at the bottom if an invalid input is given.
+function mainMenu(error){
         console.log('|======================================|');
         console.log('|               MusoPlan               |');
         console.log('|======================================|');
@@ -17,6 +22,17 @@ function mainMenu(){
         console.log('|                                      |');
         console.log('|                                      |');
         console.log('|======================================|');
+        console.log('                                        ');
+        
+        //if statment used to display error message if invalid input was used on the last loop.
+        //this technique has been used in order to display the error message beneath the menu.
+        //the alternate would be to have the error message, then the program loops and posts the menu,
+        //leaving the error message above the menu and possibly out of sight.
+        if (error == true){
+            console.log("Error: Incorrect input. Please select either [1,2,3,4,5,6,7,8 or 9]");
+        } 
 
-        choice = prompt('Enter your choice [from 1 - 9]: ');
+        return prompt('Enter your choice [from 1 - 9]: ');
 }
+//exports the functions from this page
+module.exports={mainMenu};
