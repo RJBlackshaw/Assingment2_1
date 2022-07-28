c32265d69ccbd5a04877eadaf700e6172ca173ec
RobbieB <r.blackshaw@hotmail.com>
2022-07-28 12:43:11 +1000

    add class, constructor and export to Musician.js

---
+++ Musician.js
@@ -0,0 +1,9 @@
+class Musician{
+    constructor(fullName, experience, hourlyRate){
+        this.fullName = fullName;
+        this.experience = experience;
+        this.hourlyRate=hourlyRate;
+    }
+}
+
+module.exports={Musician};
