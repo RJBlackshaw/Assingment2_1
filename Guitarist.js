8eefda190cc14f9eda6cfc2816980efc6e60490f
RobbieB <r.blackshaw@hotmail.com>
2022-08-02 14:41:27 +1000

    add detailed description function to guitarist class in guitarist.js

--- Guitarist.js
+++ Guitarist.js
@@ -3,7 +3,12 @@ const { Musician } = require("./Musician");
 class Guitarist extends Musician{
     constructor(instrument, musicianName, experience, hourlyRate){
         super(instrument, musicianName, experience, hourlyRate)
-    }   
+    }
+
+    detailedDescription(){
+        console.log(`Hi there! My name is ${this.musicianName}, I am a guitarist,\nI have been playing for ${this.experience} years and my hourly rate is $${this.hourlyRate} an hour.\nAs a flautist, I like to say:\n\n"1989 heavy metal instrument of the year" \n`);
+        console.log('--------------------------------------------\n');
+    }
 }
 
 module.exports={Guitarist};
