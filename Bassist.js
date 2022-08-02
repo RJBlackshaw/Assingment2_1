f96812b2a962680a810cfd46bc84a33c24e675b9
RobbieB <r.blackshaw@hotmail.com>
2022-08-02 13:59:05 +1000

    add detailed Description Function to Bassist class in Bassist.js

--- Bassist.js
+++ Bassist.js
@@ -4,6 +4,11 @@ class Bassist extends Musician{
     constructor(instrument, musicianName, experience, hourlyRate){
         super(instrument, musicianName, experience, hourlyRate)
     }
+
+    detailedDescription(){
+        console.log(`Hi there! My name is ${this.musicianName}, I am a bassist,\nI have been playing for ${this.experience} years and my hourly rate is $${this.hourlyRate} an hour.\nAs a bassist, I like to say:\n\n"Everyone loves a bassist" \n`);
+        console.log('--------------------------------------------\n');
+    }
 }
 
 module.exports={Bassist};
