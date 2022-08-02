bef6c67b10e38da2e234736939966d4763bfeb9f
RobbieB <r.blackshaw@hotmail.com>
2022-08-02 14:42:05 +1000

    add detailed description function to percussionist class in Percussionist.js

--- Percussionist.js
+++ Percussionist.js
@@ -4,6 +4,11 @@ class Percussionist extends Musician{
     constructor(instrument, musicianName, experience, hourlyRate){
         super(instrument, musicianName, experience, hourlyRate)
     }
+
+    detailedDescription(){
+        console.log(`Hi there! My name is ${this.musicianName}, I am a percussionist,\nI have been playing for ${this.experience} years and my hourly rate is $${this.hourlyRate} an hour.\nAs a flautist, I like to say:\n\n"1989 heavy metal instrument of the year" \n`);
+        console.log('--------------------------------------------\n');
+    }
 }
 
 module.exports={Percussionist};
