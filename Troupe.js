2d9f9af14344b0db63255da89102b9129edbcea7
RobbieB <r.blackshaw@hotmail.com>
2022-08-02 13:03:35 +1000

    add Summary description function to Troupe class in Troupe.js

--- Troupe.js
+++ Troupe.js
@@ -41,10 +41,86 @@ class Troupe{
     }
 
     tMembers = new Array();
-
+    
+    //push musician instances to tMembers array stored in troupe instances.
     addMusician(musician){
         this.tMembers.push(musician)
         console.log(`\n${musician.musicianName} has been added to ${this.troupeName}.`)
+    }
+
+    //returns troupe summary description
+    troupeDescription(){
+        let totalRate = 0;
+        let totalGuitars = 0;
+        let totalBass = 0;
+        let totalFlute = 0;
+        let totalPercussion = 0;
+
+        //adds troupe members wages together
+        for(let i=0; i < this.tMembers.length; i++){
+            totalRate += this.tMembers[i].hourlyRate;
+        }
+
+        //loop gathers total of each instrument
+        for(let i=0; i < this.tMembers.length; i++){
+            switch(this.tMembers[i].instrument){
+                case 'Guitarist':
+                    totalGuitars += 1;
+                    continue;
+
+                case 'Bassist':
+                    totalBass += 1;
+                    continue;
+
+                case 'Flautist':
+                    totalFlute += 1;
+                    continue;
+
+                case 'Percussionist':
+                    totalPercussion += 1;
+                    continue;
+            }
+        }
+
+        //Function that turns instrument totals into strings
+        function instrumentList(){
+            console.log()
+            if(totalGuitars == 1){
+                console.log(`             1 Guitarist`)
+            }
+            if (totalGuitars > 1){
+                console.log(`             ${totalGuitars} Guitarist's`)
+            }
+            if(totalFlute == 1){
+                console.log(`             1 Flautist`)
+            }
+            if (totalFlute > 1){
+                console.log(`             ${totalFlute} Flautist's`)
+            }
+            if(totalBass == 1){
+                console.log(`             1 Bassist`)
+            }
+            if (totalBass > 1){
+                console.log(`             ${totalBass} Bassist's`)
+            }
+            if(totalPercussion == 1){
+                console.log(`             1 Percussionst`)
+            }
+            if (totalPercussion > 1){
+                console.log(`             ${totalPercussion} Percussionst's`)
+            }
+        }
+
+        //summary description
+        console.log('\n|======================================|');
+        console.log(`  Troupe Name:          ${this.troupeName}  `);
+        console.log(`  Genre:                ${this.genre}  `);
+        console.log(`  Minimum booking:      ${this.minDuration} hour  `);
+        console.log(`  Maximum booking:      ${this.maxDuration} hour `);
+        console.log(`  Hourly rate:          $${totalRate}/hour \n`);
+        console.log(`             ~Instruments~          `);
+                    instrumentList()
+        console.log(`|======================================|\n`);    
     }
 }
 
