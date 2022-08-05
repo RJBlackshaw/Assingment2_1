6c964a91e559189af12a255e95d2fa81a1582b88
RobbieB <r.blackshaw@hotmail.com>
2022-08-05 13:49:54 +1000

    update formatting for Musician.js

--- Musician.js
+++ Musician.js
@@ -1,44 +1,49 @@
-class Musician{
-    set instrument(inst){
-            this.instrument_=inst;
-    }
-    get instrument(){
-        return this.instrument_
-    }
+class Musician {
+  set instrument(inst) {
+    this.instrument_ = inst;
+  }
+  get instrument() {
+    return this.instrument_;
+  }
 
-    set musicianName(mName){
-        if(mName.length >=3 && mName.length <= 30){
-            this.musician_Name=mName;
-        }
-        else {
-            console.log("\nError: Invalid input. Name must be between 3 and 30 characters");
-        }
+  set musicianName(mName) {
+    if (mName.length >= 3 && mName.length <= 30) {
+      this.musician_Name = mName;
+    } else {
+      console.log(
+        "\nError: Invalid input. Name must be between 3 and 30 characters"
+      );
     }
-    get musicianName(){
-        return this.musician_Name;
-    }
+  }
+  get musicianName() {
+    return this.musician_Name;
+  }
 
-    set experience(exp){
-        if(exp>=0){
-            this.experience_=exp;
-        } else {
-            console.log("\nError: Invalid input. Experience must be whole numbers equal or greater than 0");
-        }
+  set experience(exp) {
+    if (exp >= 0) {
+      this.experience_ = exp;
+    } else {
+      console.log(
+        "\nError: Invalid input. Experience must be whole numbers equal or greater than 0"
+      );
     }
-    get experience(){
-        return this.experience_
-    }
+  }
+  get experience() {
+    return this.experience_;
+  }
 
-    set hourlyRate(hRate){
-        if(hRate>=50){
-            this.hourly_Rate=hRate;
-        } else {
-            console.log("\nError: Invalid input. Hourly rate must be whole numbers equal or greater than 50");
-        }
+  set hourlyRate(hRate) {
+    if (hRate >= 50) {
+      this.hourly_Rate = hRate;
+    } else {
+      console.log(
+        "\nError: Invalid input. Hourly rate must be whole numbers equal or greater than 50"
+      );
     }
-    get hourlyRate(){
-        return this.hourly_Rate
-    }  
+  }
+  get hourlyRate() {
+    return this.hourly_Rate;
+  }
 }
 
-module.exports={Musician};
+module.exports = { Musician };
